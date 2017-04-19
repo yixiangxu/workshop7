ac31ebde4acd680521dd5747e68396532dc977f7
Yixiang Xu <yixiangxu@umass.edu>
2017-04-19 19:15:20 -0400

    databased

--- server/src/resetdatabase.js
+++ server/src/resetdatabase.js
@@ -143,8 +143,8 @@ function resetDatabase(db, cb) {
       // Use myself as a callback.
       resetCollection(db, collection, processNextCollection);
     } else {
-      cb();
-    }
+  addIndexes(db, cb);
+}
   }
 
   // Start processing the first collection!
@@ -173,4 +173,11 @@ if(require.main === module) {
 } else {
   // require()'d.  Export the function.
   module.exports = resetDatabase;
+}
+
+/**
+ * Adds any desired indexes to the database.
+ */
+function addIndexes(db, cb) {
+  db.collection('feedItems').createIndex({ "contents.contents": "text" }, null, cb);
 }
